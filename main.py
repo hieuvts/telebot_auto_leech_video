27874f0ab4e14c4f3ea410a3826d2e3cb0b27407
Hieu Pham <intelz.hieu@gmail.com>
2024-08-14 22:33:09 +0700

    update
    - Update and fix template that causes cannot send error message.
    - Add white list chat ID checker.
    !TODO: Setup middleware to handle whitelisted chat ID.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 from dotenv import load_dotenv
 import os
+
 load_dotenv(".env")  # take environment variables
 import logging
 import html
@@ -14,19 +15,32 @@ import yt_dlp
 from yt_dlp import YoutubeDL
 from telegram import ForceReply, Update
 from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 YT_DLP_OUTPUT_PATH = os.path.join(CURRENT_PATH, "temp.mp4")
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-DEVELOPER_CHAT_ID = os.getenv('DEVELOPER_CHAT_ID')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DEVELOPER_CHAT_ID = os.getenv("DEVELOPER_CHAT_ID")
+WHITELISTED_CHAT_ID = os.getenv("WHITELISTED_CHAT_ID").split(",")
 REGEX_MATCH_URL = re.compile(r"(?P<url>https?://[^\s]+)")
-WHITELIST_KEYWORDS = ['vt.tiktok.com', 'fb.watch', 'facebook.com/reel', 'facebook.com/watch', 'youtube.com/shorts']
+WHITELIST_KEYWORDS = [
+    "vt.tiktok.com",
+    "fb.watch",
+    "facebook.com/reel",
+    "facebook.com/watch",
+    "youtube.com/shorts",
+]
 
 MIN_LENGTH_VIDEO = 15
-MAX_LENGTH_VIDEO = 300 # Seconds
-MAX_SIZE_VIDEO = 45000000 # 45MB
-MAX_HTTPX_TIMEOUT = 120 # 120s
+MAX_LENGTH_VIDEO = 300  # Seconds
+MAX_SIZE_VIDEO = 45000000  # 45MB
+MAX_HTTPX_TIMEOUT = 120  # 120s
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.WARNING
@@ -38,48 +52,72 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
+def checkWhitelistedChatId(update: Update) -> bool:
+    if update.message is None:
+        return False
+    chatId = str(update.message.chat_id)
+    if chatId in WHITELISTED_CHAT_ID:
+        return True
+    return False
+
+
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 async def handleStartCmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        rf"Bot's alive! 🤖👋"
-    )
+    await update.message.reply_text(rf"Bot's alive! 🤖👋")
+
+
 async def handleHelpCmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         rf"Hỗ trợ các link sau: \n'vt.tiktok.com', 'fb.watch', 'facebook.com/reel', 'facebook.com/watch', 'youtube.com/shorts'"
     )
-#TODO: Fix get duration for fb vid
+
+
+# TODO: Fix get duration for fb vid
 def validate_video_length(info):
-    duration = info.get('duration')
+    duration = info.get("duration")
     if duration and duration > MAX_LENGTH_VIDEO:
-        return 'The video is too long: ' + str(duration) + " (s)"
+        return "The video is too long: " + str(duration) + " (s)"
     elif duration and duration < MIN_LENGTH_VIDEO:
-        return 'The video is too short: ' + str(duration) + " (s)"
+        return "The video is too short: " + str(duration) + " (s)"
+
 
 def download_progress_hook(d):
-    if d['status'] == 'finished':
-        print('\nDone downloading, now post-processing ...')
+    if d["status"] == "finished":
+        print("\nDone downloading, now post-processing ...")
 
-ydl_opts = { 
+
+ydl_opts = {
     # 'format': format_selector,
     # 'progress_hooks': [download_progress_hook],
     # 'format': 'bestaudio/best',
     # 'match_filter': validate_video_length, # Not work with facebook video
-    'quiet': True,
-    'allow_unplayable_formats': False,
-    'outtmpl': {'default': YT_DLP_OUTPUT_PATH},
+    "quiet": True,
+    "allow_unplayable_formats": False,
+    "outtmpl": {"default": YT_DLP_OUTPUT_PATH},
 }
 
 
-async def handleNormalMessage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def handleNormalMessage(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
     # Ignore Grafana alert
     if update.message is None:
         return
+    if checkWhitelistedChatId(update) == False:
+        # await context.bot.send_message(
+        #     chat_id=DEVELOPER_CHAT_ID,
+        #     text="Forbidden chat ID " + str(update.message.chat_id),
+        #     read_timeout=MAX_HTTPX_TIMEOUT,
+        #     write_timeout=MAX_HTTPX_TIMEOUT,
+        # )
+        print("Forbidden chat ID " + str(update.message.chat_id))
+        return False
     # Check if message contains video URL
     successFlag = 0
     errorMessage = ""
     matchedUrls = REGEX_MATCH_URL.search(update.message.text)
-    if (matchedUrls):
+    if matchedUrls:
         matchedUrl = matchedUrls.group("url")
         for keyword in WHITELIST_KEYWORDS:
             if keyword in matchedUrl:
@@ -98,14 +136,24 @@ async def handleNormalMessage(update: Update, context: ContextTypes.DEFAULT_TYPE
         #                                 reply_to_message_id=update.message.message_id)
         # Check file size
         video_size = os.path.getsize(YT_DLP_OUTPUT_PATH)
-        if (video_size >= MAX_SIZE_VIDEO):
-            await update.message.reply_text(text="Video is too large (" + str(video_size/1000000) + " MB). Only support <50MB video")
+        if video_size >= MAX_SIZE_VIDEO:
+            await update.message.reply_text(
+                text="Video is too large ("
+                + str(video_size / 1000000)
+                + " MB). Only support <50MB video"
+            )
         else:
-            await update.message.reply_video(video=YT_DLP_OUTPUT_PATH,
-                                reply_to_message_id=update.message.message_id, read_timeout=MAX_HTTPX_TIMEOUT, write_timeout=MAX_HTTPX_TIMEOUT)
+            await update.message.reply_video(
+                video=YT_DLP_OUTPUT_PATH,
+                reply_to_message_id=update.message.message_id,
+                read_timeout=MAX_HTTPX_TIMEOUT,
+                write_timeout=MAX_HTTPX_TIMEOUT,
+            )
         os.remove(YT_DLP_OUTPUT_PATH)
     elif successFlag == -1:
-        await update.message.reply_text(text="Cannot download video 🐛🐛 -> " + errorMessage)
+        await update.message.reply_text(
+            text="Cannot download video 🐛🐛 -> " + errorMessage
+        )
         print("\n======END ERROR======\n")
 
 
@@ -115,26 +163,32 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
     logger.error("Exception while handling an update:", exc_info=context.error)
     # traceback.format_exception returns the usual python message about an exception, but as a
     # list of strings rather than a single string, so we have to join them together.
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
     tb_string = "".join(tb_list)
-    tb_string = (tb_string[:4000] + '..') if len(tb_string) > 3900 else tb_string
+    tb_string = (tb_string[:4000] + "..") if len(tb_string) > 3900 else tb_string
     message = (
         "An exception was raised while handling an update\n"
-        "</pre>\n\n"
         f"<pre>{html.escape(tb_string)}</pre>"
     )
 
     try:
         # Finally, send the message
         await context.bot.send_message(
-            chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML,
-            read_timeout=MAX_HTTPX_TIMEOUT, write_timeout=MAX_HTTPX_TIMEOUT
+            chat_id=DEVELOPER_CHAT_ID,
+            text=message,
+            parse_mode=ParseMode.HTML,
+            read_timeout=MAX_HTTPX_TIMEOUT,
+            write_timeout=MAX_HTTPX_TIMEOUT,
         )
     except Exception as e:
         print("HTML Error message " + message)
         await context.bot.send_message(
-            chat_id=DEVELOPER_CHAT_ID, text="\t ⚠️⚠️Unexpected error: " + str(e),
-            read_timeout=MAX_HTTPX_TIMEOUT, write_timeout=MAX_HTTPX_TIMEOUT
+            chat_id=DEVELOPER_CHAT_ID,
+            text="\t ⚠️⚠️Unexpected error: " + str(e),
+            read_timeout=MAX_HTTPX_TIMEOUT,
+            write_timeout=MAX_HTTPX_TIMEOUT,
         )
 
 
@@ -142,7 +196,8 @@ async def test_err(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(
         chat_id=DEVELOPER_CHAT_ID, text="Error happens", parse_mode=ParseMode.HTML
     )
-    raise ValueError('A very specific bad thing happened.') 
+    raise ValueError("A very specific bad thing happened.")
+
 
 def main() -> None:
     """Start the bot."""
@@ -155,11 +210,14 @@ def main() -> None:
     application.add_handler(CommandHandler("test", test_err))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handleNormalMessage))
+    application.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, handleNormalMessage)
+    )
     application.add_error_handler(error_handler)
     print("---Start---\n")
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.MESSAGE)
+
 
 if __name__ == "__main__":
     main()
